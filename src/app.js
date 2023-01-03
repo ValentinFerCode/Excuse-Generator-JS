7960d4992219f992b1f626d619d99ae8bd62d037
xControlFreak <xsimplemail@gmail.com>
2023-01-03 16:28:35 +0000

    Excuse-Generator-JS

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function refresh() {
+    let nAleatorio1 = Math.floor(Math.random() * who.length);
+    let nAleatorio2 = Math.floor(Math.random() * action.length);
+    let nAleatorio3 = Math.floor(Math.random() * what.length);
+    let nAleatorio4 = Math.floor(Math.random() * when.length);
+
+    let positionsWho = who[nAleatorio1];
+    let positionsAction = action[nAleatorio2];
+    let positionsWhat = what[nAleatorio3];
+    let positionsWhen = when[nAleatorio4];
+
+    let excuse =
+      positionsWho +
+      " " +
+      positionsAction +
+      " " +
+      positionsWhat +
+      " " +
+      positionsWhen;
+
+    return excuse;
+  }
+
+  document.getElementById("excuses").innerHTML = refresh();
+
+  console.log(refresh());
 };
